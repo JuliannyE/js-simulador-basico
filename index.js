eeffd99d122ffeb885cf9bbb11c35a4f1596790d
julianny <julianny.estrada@gmail.com>
2023-05-11 22:30:22 -0400

    agregar funciones

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-let ejecutar = true
+function solicitarNumero() {
+    const numero = prompt("Por favor, ingrese un número positivo")
+    return numero
+}
 
-while (ejecutar) {
+function saludar(msg) {
+    alert(msg)
+}
 
-    const entrada = prompt("Por favor, ingrese un número positivo")
 
-    if (entrada > 0) {
-        for (let i = 1; i <= entrada; i++) {
-            const mensaje = "Intento #" + i + ": Hola!"
-            alert(mensaje)
+function interactuar() {
+    let ejecutar = true
+
+    while (ejecutar == true) {
+
+        const entrada = solicitarNumero()
+
+        if (entrada > 0) {
+            for (let i = 1; i <= entrada; i++) {
+                const mensaje = "Intento #" + i + ": Hola!"
+                saludar(mensaje)
+            }
+
+            ejecutar = false
+        } else {
+            const error = "Ingrese un número válido"
+            saludar(error)
         }
 
-        ejecutar = false
-    } else {
-        alert("Ingrese un número válido")
     }
-
 }
+
+interactuar()
